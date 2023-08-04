5b61be31faaa651b5c44d8efbe857f802c40c383
Dudkin Sergey <dsergik@mail.ru>
2023-08-04 06:47:57 +0300

    Хук useEffect Добавление класса к body и кнопке.

--- src/Layout/BtnDarkMode/BtnDarkMode.jsx
+++ src/Layout/BtnDarkMode/BtnDarkMode.jsx
@@ -6,19 +6,31 @@ import './style.scss';
 const BtnDarkMode = () => {
   const [darkMode, setDarkMode] = React.useState('light');
 
+  React.useEffect(() => {
+    console.log('useEffect Fired');
+    console.log({ darkMode });
+
+    if (darkMode === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   const toggleDarkMode = () => {
-    console.log('+');
     setDarkMode((currentValue) => {
       return currentValue === 'light' ? 'dark' : 'light';
     });
   };
+  const btnNormal = 'dark-mode-btn';
+  const btnActive = 'dark-mode-btn dark-mode-btn--active';
 
   return (
     <>
-    {darkMode}
-      <button className="dark-mod-btn" onClick={toggleDarkMode}>
-        <img className="dark-mod-btn__icon" src={sun} alt="Light mode" />
-        <img className="dark-mod-btn__icon" src={moon} alt="Dark mode" />
+      {darkMode}
+      <button className={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
+        <img className="dark-mode-btn__icon" src={sun} alt="Light mode" />
+        <img className="dark-mode-btn__icon" src={moon} alt="Dark mode" />
       </button>
     </>
   );
