5e56a906639fa7480f222d4dde2891df12c54f4c
Dudkin Sergey <dsergik@mail.ru>
2023-08-04 05:32:10 +0300

    useState Состояние для темной темы

--- src/Layout/BtnDarkMode/BtnDarkMode.jsx
+++ src/Layout/BtnDarkMode/BtnDarkMode.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import lightMode from '../../assets/icons/sun.svg';
-import darkMode from '../../assets/icons/moon.svg';
-import './style.scss'
+import sun from '../../assets/icons/sun.svg';
+import moon from '../../assets/icons/moon.svg';
+import './style.scss';
 
 const BtnDarkMode = () => {
+  const [darkMode, setDarkMode] = React.useState('light');
 
   const toggleDarkMode = () => {
     console.log('+');
-  }
+    setDarkMode((currentValue) => {
+      return currentValue === 'light' ? 'dark' : 'light';
+    });
+  };
 
   return (
-    <button className="dark-mod-btn" onClick={toggleDarkMode}>
-              <img
-                className="dark-mod-btn__icon"
-                src={lightMode}
-                alt="Light mode"
-              />
-              <img
-                className="dark-mod-btn__icon"
-                src={darkMode}
-                alt="Dark mode"
-              />
-            </button>
-  )
-}
+    <>
+    {darkMode}
+      <button className="dark-mod-btn" onClick={toggleDarkMode}>
+        <img className="dark-mod-btn__icon" src={sun} alt="Light mode" />
+        <img className="dark-mod-btn__icon" src={moon} alt="Dark mode" />
+      </button>
+    </>
+  );
+};
 
 export default BtnDarkMode;
